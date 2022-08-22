a23f582f562193dd85c7d398c0ee7615fb0720ab
code-death <poorvanshkavta938@gmial.com>
2022-08-22 11:41:46 +0530

    Made all the blogs availaible on home

---
+++ components/Posts.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { Post } from '../typing'
+import Link from 'next/link'
+import { urlFor } from '../sanity'
+
+interface Props {
+  posts: Post[]
+}
+
+function Posts({ posts }: Props) {
+  console.log(posts)
+  return (
+    <div className='grid grid-cols-1 sm:grid-cols-2 px-5 lg:grid-cols-3 gap-3 md:gap-6 py-6 p-2 max-w-7xl'>
+      {posts.map(post => (
+        <Link key={post._id} href={`/post/${post.slug.current}`}>
+          <div className='border rounded-lg group cursor-pointer overflow-hidden'>
+            <img
+              className='h-60 w-full object-cover group-hover:scale-105 transition-transform duration-200 ease-in-out'
+              src={urlFor(post.mainImage).url()!}
+              alt={post.slug.current}
+            />
+            <div className='flex justify-between p-7 bg-white'>
+              <div className=''>
+                <p className='text-lg font-bold'>{post.title}</p>
+                <p className='text-xs'>
+                  {post.description} by {post.author.name}{' '}
+                </p>
+              </div>
+              <img
+                className='h-12 w-12 rounded-full'
+                src={urlFor(post.author.image).url()!}
+                alt={post.author.name}
+              />
+            </div>
+          </div>
+        </Link>
+      ))}
+    </div>
+  )
+}
+
+export default Posts
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,17 +2,45 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import BlogPost from '../components/BlogPost'
 import Header from '../components/Header'
+import { sanityClient, urlFor } from '../sanity'
+import { Post } from '../typing'
+import Posts from '../components/Posts'
 
-const Home: NextPage = () => {
+interface Props {
+  posts: Post[]
+}
+
+export default function Home({ posts }: Props) {
   return (
-    <div className=''>
+    <div className='max-w-7xl mx-auto'>
       <Head>
         <title>Medium Blog</title>
       </Head>
       <Header />
       <BlogPost />
+      <Posts posts={posts} />
     </div>
   )
 }
 
-export default Home
+export const getServerSideProps = async () => {
+  const query = `*[_type == 'post']{
+    _id,
+    title,
+    author-> {
+      name,
+      image
+    },
+    description,
+    mainImage,
+    slug
+  }`
+
+  const posts = await sanityClient.fetch(query)
+
+  return {
+    props: {
+      posts,
+    },
+  }
+}

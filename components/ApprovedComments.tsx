bf460702df71afd223408b549cc7cffa8bbfb750
code-death <poorvanshkavta938@gmial.com>
2022-08-22 21:55:25 +0530

    Deployed sanity

--- components/ApprovedComments.tsx
+++ components/ApprovedComments.tsx
@@ -6,22 +6,25 @@ interface Props {
 }
 
 function ApprovedComments({ post }: Props) {
-  console.log(post)
   return (
-    <div className='flex flex-col p-10 my-10 max-w-2xl mx-auto shadow-yellow-500 shadow space-y-2'>
-      <h3 className='text-4xl'>Comments</h3>
-      <hr className='pb-2' />
-      {post.comments.map(comment => {
-        return (
-          <div key={comment._id}>
-            <p>
-              <span className='text-yellow-500'>{comment.name}: </span>
-              {comment.comment}
-            </p>
-          </div>
-        )
-      })}
-    </div>
+    <>
+      {post.comments.length !== 0 ? (
+        <div className='flex flex-col p-10 my-10 max-w-2xl mx-auto shadow-yellow-500 shadow space-y-2'>
+          <h3 className='text-4xl'>Comments</h3>
+          <hr className='pb-2' />
+          {post.comments.map(comment => {
+            return (
+              <div key={comment._id}>
+                <p>
+                  <span className='text-yellow-500'>{comment.name}: </span>
+                  {comment.comment}
+                </p>
+              </div>
+            )
+          })}
+        </div>
+      ) : null}
+    </>
   )
 }
 
